c0249b0c701c31a8dd9b9eb809f158742c539739
Marcus Loosen <mail@marcus-loosen.de>
2024-11-15 12:23:11 +0100

    redesign mainpage, chat main and workspace-menu

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-mainpage',
   templateUrl: './mainpage.component.html',
-  styleUrls: ['./mainpage.component.scss']
+  styleUrls: ['./mainpage.component.scss'],
 })
-export class MainpageComponent {
-
-}
+export class MainpageComponent {}
