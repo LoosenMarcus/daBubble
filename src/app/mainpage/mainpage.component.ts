162b84e0453a7fa8a8a163ce741ff55d24445bd0
LoosenMarcus <mail@marcus-loosen.de>
2024-11-16 16:16:52 +0100

    emoji loading addded

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -8,4 +8,32 @@ import { ChatSmallComponent } from '../chat-small/chat-small.component';
   templateUrl: './mainpage.component.html',
   styleUrls: ['./mainpage.component.scss'],
 })
-export class MainpageComponent {}
+export class MainpageComponent {
+  variable = true;
+  json = [
+    {
+      id: 1,
+      name: 'Marcus',
+      alter: 41,
+    },
+    {
+      id: 2,
+      name: 'Marcus2',
+      alter: 412,
+    },
+  ];
+  constructor() {
+    // this.loadEmojis();
+    console.log(this.json);
+  }
+
+  /**
+   * Load all Emojis
+   */
+  // async loadEmojis() {
+  //   let emoji = await fetch(
+  //     `https://emoji-api.com/emojis?access_key=2b3522d2007654b0ef6dfc90c2c5c4cd74d62247`
+  //   );
+  //   this.emojiJson.push(await emoji.json());
+  // }
+}
