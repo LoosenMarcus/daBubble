b1d2d9ceff3418cd59577368054557748a916e10
LoosenMarcus <mail@marcus-loosen.de>
2024-11-17 11:31:23 +0100

    add channelPopUp and add UserPopup add and function created

--- src/app/workspace-menu/workspace-menu.component.ts
+++ src/app/workspace-menu/workspace-menu.component.ts
@@ -5,4 +5,34 @@ import { Component } from '@angular/core';
   templateUrl: './workspace-menu.component.html',
   styleUrls: ['./workspace-menu.component.scss'],
 })
-export class WorkspaceMenuComponent {}
+export class WorkspaceMenuComponent {
+  addChannelPopUpStatus = false;
+  addUserPopUpStatus = false;
+
+  toggleAddChannelPopUp() {
+    if (!this.addChannelPopUpStatus) {
+      this.addChannelPopUpStatus = true;
+    } else {
+      this.addChannelPopUpStatus = false;
+    }
+  }
+
+  toggleAddUserPopUp() {
+    if (!this.addChannelPopUpStatus) {
+      this.addUserPopUpStatus = true;
+      this.addChannelPopUpStatus = false;
+    } else {
+      this.addUserPopUpStatus = false;
+    }
+  }
+
+  addUserPopUp() {
+    // saveChannel();
+    this.addChannelPopUpStatus = false;
+    if (!this.addUserPopUpStatus) {
+      this.addChannelPopUpStatus = true;
+    } else {
+      this.addUserPopUpStatus = false;
+    }
+  }
+}
