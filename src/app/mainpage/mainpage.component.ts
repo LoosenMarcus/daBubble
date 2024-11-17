5ecfa21f6af813fcd507ddda99eac11925eb17c2
LoosenMarcus <mail@marcus-loosen.de>
2024-11-17 09:42:03 +0100

    Popup Menu add for user

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -9,31 +9,13 @@ import { ChatSmallComponent } from '../chat-small/chat-small.component';
   styleUrls: ['./mainpage.component.scss'],
 })
 export class MainpageComponent {
-  variable = true;
-  json = [
-    {
-      id: 1,
-      name: 'Marcus',
-      alter: 41,
-    },
-    {
-      id: 2,
-      name: 'Marcus2',
-      alter: 412,
-    },
-  ];
-  constructor() {
-    // this.loadEmojis();
-    // console.log(this.json);
-  }
+  openUserMenuStatus = false;
 
-  /**
-   * Load all Emojis
-   */
-  // async loadEmojis() {
-  //   let emoji = await fetch(
-  //     `https://emoji-api.com/emojis?access_key=2b3522d2007654b0ef6dfc90c2c5c4cd74d62247`
-  //   );
-  //   this.emojiJson.push(await emoji.json());
-  // }
+  openUserMenu() {
+    if (!this.openUserMenuStatus) {
+      this.openUserMenuStatus = true;
+    } else {
+      this.openUserMenuStatus = false;
+    }
+  }
 }
