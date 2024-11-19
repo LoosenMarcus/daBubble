2a90b3fcb11c2f375600dd85bbebefda14d8a7b3
LoosenMarcus <mail@marcus-loosen.de>
2024-11-19 09:30:22 +0100

    Service Toggle Menus add

--- src/app/chat-small/chat-small.component.ts
+++ src/app/chat-small/chat-small.component.ts
@@ -1,10 +1,8 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-chat-small',
   templateUrl: './chat-small.component.html',
   styleUrls: ['./chat-small.component.scss'],
 })
-export class ChatSmallComponent {
-  isHideSmallChatContainer = false;
-}
+export class ChatSmallComponent {}
